20d87c7264eebfdec099d43a275c3d3e798380d8
banea-radu <100232231+banea-radu@users.noreply.github.com>
2022-12-27 00:49:40 +0200

    button component update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 
 import { HomeComponent } from './component/home/home.component';
 import { AboutComponent } from './component/about/about.component';
 import { ContactComponent } from './component/contact/contact.component';
+import { CommonModule } from '@angular/common';
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64],
+};
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  // {path: '', component: HomeComponent},
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'slider', component: ContactComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/component/shared/button/button.component.ts
+++ src/app/component/shared/button/button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
+  
+  @Input()
+  title: string= '';
 
+  constructor(private viewportScroller: ViewportScroller) { }
+  
+  scrollToAnchroingPosition(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
+  
 }
