c85707d07b43cc3a2873200207adf7eef9215c40
banea-radu <100232231+banea-radu@users.noreply.github.com>
2022-12-30 18:03:51 +0200

    home cards component adjustment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/shared/header/header.component';
 import { FooterComponent } from './component/shared/footer/footer.component';
 import { ButtonComponent } from './component/shared/button/button.component';
-import { CardComponent } from './component/shared/card/card.component';
+import { CardsHomeComponent } from './component/shared/cards-home/cards-home.component';
 import { HomeComponent } from './component/home/home.component';
 import { AboutComponent } from './component/about/about.component';
 import { ContactComponent } from './component/contact/contact.component';
@@ -17,7 +17,7 @@ import { ContactComponent } from './component/contact/contact.component';
     HeaderComponent,
     FooterComponent,
     ButtonComponent,
-    CardComponent,
+    CardsHomeComponent,
     HomeComponent,
     AboutComponent,
     ContactComponent
